44f93111f7c2fa67d10541525288204adac97126
Breno <onerbreno@outlook.com>
2023-02-02 22:40:24 -0300

    :tada: initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const modal = document.querySelector('.modal-overlay')
+
+function openModal() {
+    modal.classList.add('active')
+}
+function closeModal() {
+    modal.classList.remove('active')
+}
+
